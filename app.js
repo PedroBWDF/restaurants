eb2111aa9ed72eb2560ffc0835815c194fa3df2c
PedroBWDF <bwdf64587@gmail.com>
2024-01-08 17:28:01 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
-// const { engine } = require('express-handlebars')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+
+// import express from 'express';
+// import { engine } from 'express-handlebars';
+// app.engine('handlebars', engine());
+
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 //根據express文件，載入餐廳json靜態資料
 app.use(express.static('public'))
@@ -11,7 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('listing')
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res) => {
