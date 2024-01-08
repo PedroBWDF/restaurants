7df50d2de1c2747083192fc59e2e2cbd6ed1aef1
PedroBWDF <bwdf64587@gmail.com>
2024-01-08 17:07:49 +0800

    build: project init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+// const { engine } = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
