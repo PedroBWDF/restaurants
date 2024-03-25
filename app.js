9bf047718efc68022e95868a06387460b6ce78fc
PedroBWDF <bwdf64587@gmail.com>
2024-03-25 21:18:52 +0800

    implement delete restaurant

--- app.js
+++ app.js
@@ -155,6 +155,13 @@ app.put('/restaurants/:id', (req, res) => {
   .then(() => res.redirect(`/restaurants/${id}`))
 })
 
+app.delete('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+
+  return Restaurant.destroy({ where: { id } })
+    .then(() => res.redirect('/restaurants'))
+})
+
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
