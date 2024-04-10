2494ec077b275ca0ab812f7fc3595c318fe58c4b
PedroBWDF <bwdf64587@gmail.com>
2024-04-10 14:31:33 +0800

    use express router

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+// 準備引入路由模組
+const restaurants = require('./restaurants')
+
+//第一個參數以'/restaurants'作為根路徑，所有導向'/restaurants'的請求都會再分發至第二個參數'restaurants'的文件
+router.use('/restaurants', restaurants)
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+// 匯出路由器
+module.exports = router
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,46 +1,26 @@
 const express = require('express')
-const { engine } = require('express-handlebars')
-const app = express()
-const port = 3000
-const methodOverride = require('method-override')
-// 引用路由器
-const router = require('./routes') 
+const router = express.Router()
 
-
-const db = require('./models')
+const db = require('../models')
 const Restaurant = db.Restaurant
-
-app.engine('.hbs', engine({ extname: '.hbs' }))
-app.set('view engine', '.hbs');
-app.set('views', './views');
-
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-
-//根據express文件，載入餐廳json靜態資料
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.redirect('/restaurants')
-})
 
 const { Op } = require('sequelize');
 
-app.get('/restaurants', (req, res) => {
+router.get('/', (req, res) => {
   //使用query方法取得搜尋欄的關鍵字
-  const keyword = req.query.keyword 
+  const keyword = req.query.keyword
 
   let matchedRestaurantsPromise;
 
   if (keyword) {
+    //sequelize官方文件有針對Op.or和Op.like說明
     matchedRestaurantsPromise = Restaurant.findAll({
-      //sequelize官方文件有針對Op.or和Op.like說明
       where: {
         [Op.or]: [
           { name: { [Op.like]: `%${keyword}%` } },
           { category: { [Op.like]: `%${keyword}%` } }
         ]
-      },
+      }, 
       raw: true
     });
   } else {
@@ -51,7 +31,7 @@ app.get('/restaurants', (req, res) => {
     .then((matchedRestaurants) => {
       // restaurants和keyword為渲染模板時用的名稱，可見index.hbs檔案
       res.render('index', { restaurants: matchedRestaurants, keyword });
-    })
+    }) 
     .catch((error) => {
       console.error('Error fetching restaurants:', error);
       res.status(500).send('Error fetching restaurants');
@@ -59,29 +39,25 @@ app.get('/restaurants', (req, res) => {
 });
 
 
-app.get('/restaurants/new', (req, res) => {
+router.get('/new', (req, res) => {
   return res.render('new')
 })
 
-app.post('/restaurants', (req, res) => {
-  //利用req.body拿到POST方法傳過來的資料，會是一個物件，name為key、value為值
-  const name = req.body.name 
-  const category = req.body.category
-  const location = req.body.location
-  const phone = req.body.phone
-  const image = req.body.image
+router.post('/', (req, res) => {
+  //利用req.body拿到POST方法傳過來的資料，會是一個物件，name為key、value為值。下面宣告用解構賦值
+  const { name, category, location, phone, image } = req.body
 
   return Restaurant.create({ name, category, location, phone, image })
     .then(() => res.redirect('/restaurants'))
     .catch((err) => console.log(err))
 })
 
-app.get('/restaurants/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findByPk(id, {
     attributes: ['id', 'name', 'category', 'location', 'phone', 'image'],
-    raw: true 
-  }) //有沒有return都可，but why?
+    raw: true
+  }) 
     //findByPk查詢到的資料會被作為參數傳遞到.then的callback function
     .then((restaurant) => { 
       if (!restaurant) {
@@ -94,44 +70,44 @@ app.get('/restaurants/:id', (req, res) => {
     .catch((err) => res.status(422).json(err))
 })
 
-app.get('/restaurants/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   const id = req.params.id
-  return Restaurant.findByPk(id, { 
+  return Restaurant.findByPk(id, {
     attributes: ['id', 'name', 'category', 'location', 'phone', 'image'],
-  raw: true 
-})
+    raw: true
+  })
     //findByPk查詢到的資料會被作為參數傳遞到.then的callback function
-    .then((restaurant) => {
+    .then((restaurant) => { 
       if (!restaurant) {
         res.status(404).send('Restaurant not found')
       }
       //restaurant為渲染模板時用的名稱
-      res.render('edit', { restaurant })
+      res.render('edit', { restaurant }) 
     })
 
     .catch((err) => res.status(422).json(err))
 })
 
-app.put('/restaurants/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const id = req.params.id
-  const body = req.body
+  // 下面用解構賦值寫法
+  const { name, category, location, phone, image } = req.body
 
-  Restaurant.update({ name: body.name, category: body.category, location: body.location, phone: body.phone, image: body.image }, {
+  Restaurant.update({ name, category, location, phone, image }, {
     where: { id }
   })
-  .then(() => res.redirect(`/restaurants/${id}`))
+    .then(() => res.redirect(`/restaurants/${id}`))
+
+    .catch((err) => res.status(422).json(err))
 })
 
-app.delete('/restaurants/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = req.params.id
 
   return Restaurant.destroy({ where: { id } })
     .then(() => res.redirect('/restaurants'))
+
+    .catch((err) => res.status(422).json(err))
 })
 
-// 將 request 導入路由器
-app.use(router) 
-
-app.listen(port, () => {
-  console.log(`express server is running on http://localhost:${port}`)
-})
+module.exports = router
