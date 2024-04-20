d35627a9434da3ba2ecc6a27ed65fa6f6f4bb92c
PedroBWDF <bwdf64587@gmail.com>
2024-04-20 15:11:15 +0800

    add user-restaurant relationship via migrations and implement auth check in restaurants features

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,15 +6,16 @@ const Restaurant = db.Restaurant
 
 const { Op } = require('sequelize');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     const keyword = req.query.keyword
+    const userId = req.user.id
     const sort = req.query.sort
     let sort1 = 0
     let sort2 = 0
     let sort3 = 0
     let sort4 = 0
 
-    //若被選中，賦值1 (index.hbs有定義是否被選中)
+    // 若被選中，賦值1 (index.hbs有定義是否被選中)
     if (sort === 'name_asc') {
       sort1 = 1
     } else if (sort === 'name_desc') {
@@ -48,10 +49,11 @@ router.get('/', (req, res) => {
       })
     } else {
       restaurantsPromise = Restaurant.findAll({
+        where : { userId },
         //sequelize提供的排序方法
         order: sortOptions[sort],
         raw: true
-      });
+      })
     }
 
     restaurantsPromise
@@ -75,11 +77,12 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   //利用req.body拿到POST方法傳過來的資料，會是一個物件，name為key、value為值。下面宣告用解構賦值
   const { name, category, location, phone, image } = req.body
+  const userId = req.user.id
 
-  return Restaurant.create({ name, category, location, phone, image })
+  return Restaurant.create({ name, category, location, phone, image, userId })
     .then(() => {
       req.flash('success', '成功新增餐廳了!')
       res.redirect('/restaurants')
@@ -91,16 +94,24 @@ router.post('/', (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   const id = req.params.id
+  const userId = req.user.id
+
   return Restaurant.findByPk(id, {
-    attributes: ['id', 'name', 'category', 'location', 'phone', 'image'],
+    attributes: ['id', 'name', 'category', 'location', 'phone', 'image', 'userId'],
     raw: true
   })
     //findByPk查詢到的資料會被作為參數傳遞到.then的callback function
     .then((restaurant) => {
       if (!restaurant) {
-        res.status(404).send('Restaurant not found')
+        req.flash('error', '找不到資料')
+        return res.redirect('/restaurants')
+      }
+
+      if (restaurant.userId !== userId) {
+        req.flash('error', '你沒有存取此資料的權限')
+        return res.redirect('/restaurants')
       }
       //restaurant為渲染模板時用的名稱
       res.render('detail', { restaurant })
@@ -112,16 +123,24 @@ router.get('/:id', (req, res) => {
     })
 })
 
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', (req, res, next) => {
   const id = req.params.id
+  const userId = req.user.id
+
   return Restaurant.findByPk(id, {
-    attributes: ['id', 'name', 'category', 'location', 'phone', 'image'],
+    attributes: ['id', 'name', 'category', 'location', 'phone', 'image', 'userId'],
     raw: true
   })
     //findByPk查詢到的資料會被作為參數傳遞到.then的callback function
     .then((restaurant) => {
       if (!restaurant) {
-        res.status(404).send('Restaurant not found')
+        req.flash('error', '找不到資料')
+        return res.redirect('/restaurants')
+      }
+
+      if (restaurant.userId !== userId) {
+        req.flash('error', '你沒有存取此資料的權限')
+        return res.redirect('/restaurants')
       }
       //restaurant為渲染模板時用的名稱
       res.render('edit', { restaurant })
@@ -133,17 +152,32 @@ router.get('/:id/edit', (req, res) => {
     })
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   const id = req.params.id
   // 下面用解構賦值寫法
   const { name, category, location, phone, image } = req.body
+  const userId = req.user.id
 
-  Restaurant.update({ name, category, location, phone, image }, {
-    where: { id }
+  return Restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'category', 'location', 'phone', 'image', 'userId'],
   })
-    .then(() => {
-      req.flash('success', '成功更新餐廳資料!')
-      res.redirect(`/restaurants/${id}`)
+
+    .then((restaurant) => {
+      if (!restaurant) {
+        req.flash('error', '找不到資料')
+        return res.redirect('/restaurants')
+      }
+
+      if (restaurant.id !== userId) {
+        req.flash('error', '你沒有存取此資料的權限')
+        return res.redirect('/restaurants')
+      }
+      
+      return restaurant.update({ name, category, location, phone, image })
+        .then(() => {
+          req.flash('success', '成功更新餐廳資料!')
+          res.redirect(`/restaurants/${id}`)
+        })
     })
 
     .catch((error) => {
@@ -152,17 +186,34 @@ router.put('/:id', (req, res) => {
     })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   const id = req.params.id
+  const userId = req.user.id
 
-  return Restaurant.destroy({ where: { id } })
-    .then(() => {
-      req.flash('success', '餐廳資料已被刪除!')
-      res.redirect('/restaurants')
+  return Restaurant.findByPk(id, {
+    attributes: ['id', 'name', 'category', 'location', 'phone', 'image', 'userId'],
+  })
+
+    .then((restaurant) => {
+      if (!restaurant) {
+        req.flash('error', '找不到資料')
+        return res.redirect('/restaurants')
+      }
+
+      if (restaurant.id !== userId) {
+        req.flash('error', '你沒有存取此資料的權限')
+        return res.redirect('/restaurants')
+      }
+
+      return restaurant.destroy()
+        .then(() => {
+          req.flash('success', '餐廳資料已被刪除!')
+          res.redirect('/restaurants')
+        })
     })
 
     .catch((error) => {
-      error.errorMessage = '資料取得失敗:('
+      error.errorMessage = '刪除失敗:('
       next(error)
     })
 })
